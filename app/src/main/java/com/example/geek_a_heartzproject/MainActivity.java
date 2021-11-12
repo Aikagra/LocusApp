1ba20645e9f43cc7bf6b965cb4d087ef0a04929b
Aikagra <r22619aikagra@dpsrkp.net>
2021-11-12 16:45:31 +0530

    Added Login Functionality

--- app/src/main/java/com/example/geek_a_heartzproject/MainActivity.java
+++ app/src/main/java/com/example/geek_a_heartzproject/MainActivity.java
@@ -2,29 +2,40 @@ package com.example.geek_a_heartzproject;
 
 
 import android.app.ActivityOptions;
-import android.content.Context;
 import android.content.Intent;
-import android.media.AudioManager;
-import android.media.SoundPool;
 import android.os.Build;
 import android.os.Bundle;
 import android.transition.Explode;
+import android.util.Patterns;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.airbnb.lottie.LottieAnimationView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class MainActivity extends AppCompatActivity {
 
-  Button registerBtn, loginBtn;
-
-  SoundPool soundPool;
-  float volume;
-
+    private Button registerBtn, loginBtn;
+    private EditText loginEmail, loginPw;
+    private LottieAnimationView lottieAnimationView;
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -38,36 +49,82 @@ public class MainActivity extends AppCompatActivity {
 
         registerBtn = findViewById(R.id.registerBtn);
         loginBtn = findViewById(R.id.loginBtn);
-        AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-        volume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
-        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 1);
+        lottieAnimationView = findViewById(R.id.animationLottie);
+        loginEmail = findViewById(R.id.loginEmail);
+        loginPw = findViewById(R.id.loginPw);
+        mAuth = FirebaseAuth.getInstance();
 
-
-
-
-        registerBtn.setOnClickListener(view -> {
-            soundPool.play(R.raw.click, volume, volume, 1, 0, 1);
-            Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                startActivity(intent,
-                        ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
-
-            }else {
-                startActivity(intent);
-            }
-
-        });
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                soundPool.play(R.raw.click, volume, volume, 1, 0, 1);
-                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-                startActivity(intent);
+                userLogin();
+            }
+
+            private void userLogin() {
+                String email = loginEmail.getText().toString().trim();
+                String password = loginPw.getText().toString().trim();
+
+                if (email.isEmpty()){
+                    loginEmail.setError("Email is Required");
+                    loginEmail.requestFocus();
+                    return;
+                }
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    loginEmail.setError("Add a valid email");
+                    loginEmail.requestFocus();
+                    return;
+                }
+                if (password.isEmpty()){
+                    loginPw.setError("Password is Required");
+                    loginPw.requestFocus();
+                    return;
                 }
 
+                lottieAnimationView.setVisibility(View.VISIBLE);
+
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+
+                            startActivity(new Intent(MainActivity.this, HomeActivity.class));
+
+                        } else {
+                            Toast.makeText(MainActivity.this, "Failed to login, please check the credentials", Toast.LENGTH_LONG).show();
+                            lottieAnimationView.setVisibility(View.GONE);
+
+                        }
+                    }
+                });
+            }
         });
-        }
+
+
+
+        registerBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+            lottieAnimationView.setVisibility(View.VISIBLE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                startActivity(intent,
+                        ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+
+            } else {
+
+                lottieAnimationView.setVisibility(View.INVISIBLE);
+            }
+
+        });
+
 
     }
+
+
+
+    }
+
+
+
+
+
 
